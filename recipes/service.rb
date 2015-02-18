ffb136ebf03e77ad62ffebca1b4d9bd8ee7c916a
Andy Fleener <andrew.fleener@sportngin.com>
2015-02-18 14:32:34 -0600

    Fix haproxy reload

--- recipes/service.rb
+++ recipes/service.rb
@@ -1,1 +1,4 @@
-include_recipe 'haproxy::service'
+service "haproxy" do
+  supports :restart => true, :status => true, :reload => true
+  action :nothing # only define so that it can be restarted if the config changed
+end
